bc4e75faaedba43dadf6b7061a97326577a9d3ae
Maxguzman436 <max.guzman436@al.unieduk.com.br>
2026-03-10 20:16:28 -0300

    Update Matematico.py

--- Matematico.py
+++ Matematico.py
@@ -2,129 +2,191 @@
 #10/03/2026
 #Max Guzman
 
-operacao = input("selecione a operação:\n\n1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO\n5 - POTENCIAÇÃO\n6 - RADICIAÇÃO\n\nEscolha(numero): ")
-operacao = int(operacao)
+import os
 
-if operacao == 1:
-	#SOMA
-	print("\n\n")
+est = 0
+while est == 0:
 	print("#########################################")
-	print("                 SOMADOR                 ")
+	print("               Calculadora               ")
 	print("#########################################")
+	operacao = input("Selecione a operação:\n\n1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO\n5 - POTENCIAÇÃO\n6 - RADICIAÇÃO\n\nEscolha(numero): ")
+	operacao = int(operacao)
 
-	a = input("Digite o primeiro numero: ")
+	if operacao == 1:
+		#SOMA
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 SOMADOR                 ")
+		print("#########################################")
 
-	a = int(a)
+		a = input("Digite o primeiro numero: ")
 
-	b = input("Digite o segundo numero: ")
+		a = int(a)
 
-	b = int(b)
+		b = input("Digite o segundo numero: ")
 
-	c = a + b
+		b = int(b)
 
-	print(f"A soma de {a} e {b} é {c}")
+		c = a + b
 
-	input()
-elif operacao == 2:
-	#SUBTRAÇÃO
-	print("\n\n")
-	print("#########################################")
-	print("                 SUBTRAIR                ")
-	print("#########################################")
+		print(f"A soma de {a} e {b} é {c}")
+
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	elif operacao == 2:
+		#SUBTRAÇÃO
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 SUBTRAIR                ")
+		print("#########################################")
 	
-	a = input("Digite o primeiro numero: ")
+		a = input("Digite o primeiro numero: ")
 	
-	a = int(a)
+		a = int(a)
 	
-	b = input("Digite o segundo numero: ")
+		b = input("Digite o segundo numero: ")
 	
-	b = int(b)
+		b = int(b)
 	
-	c = a - b
+		c = a - b
 		
-	print(f"A subtração de {a} e {b} é {c}")
+		print(f"A subtração de {a} e {b} é {c}")
 	
-	input()
-elif operacao == 3:
-	#MULTIPLICACAO
-	print("\n\n")
-	print("#########################################")
-	print("                 MULTIPLICACAO           ")
-	print("#########################################")
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	elif operacao == 3:
+		#MULTIPLICACAO
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 MULTIPLICACAO           ")
+		print("#########################################")
 
-	a = input("Digite o primeiro numero: ")
+		a = input("Digite o primeiro numero: ")
 
-	a = int(a)
+		a = int(a)
 
-	b = input("Digite o segundo numero: ")
+		b = input("Digite o segundo numero: ")
 
-	b = int(b)
+		b = int(b)
 
-	c = a*b
+		c = a*b
 
-	print(f"A multiplicação de {a} por {b} é {c}")
+		print(f"A multiplicação de {a} por {b} é {c}")
 
-	input()
-elif operacao == 4:
-	#DIVISAO
-	print("\n\n")
-	print("#########################################")
-	print("                 DIVISAO                 ")
-	print("#########################################")
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	elif operacao == 4:
+		#DIVISAO
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 DIVISAO                 ")
+		print("#########################################")
 
-	a = input("Digite o primeiro numero: ")
+		a = input("Digite o primeiro numero: ")
 
-	a = int(a)
+		a = int(a)
 
-	b = input("Digite o segundo numero: ")
+		b = input("Digite o segundo numero: ")
 
-	b = int(b)
+		b = int(b)
 
-	c = a/b
+		c = a/b
 
-	print(f"A divisão de {a} por {b} é {c}")
+		print(f"A divisão de {a} por {b} é {c}")
 
-	input()
-elif operacao == 5:
-	#POTENCIACAO
-	print("\n\n")
-	print("#########################################")
-	print("                 POTENCIACAO             ")
-	print("#########################################")
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	elif operacao == 5:
+		#POTENCIACAO
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 POTENCIACAO             ")
+		print("#########################################")
 
-	a = input("Digite o numero: ")
+		a = input("Digite o numero: ")
 
-	a = int(a)
+		a = int(a)
 
-	b = input("Digite a potencia: ")
+		b = input("Digite a potencia: ")
 
-	b = int(b)
+		b = int(b)
 
-	c = a**b
+		c = a**b
 
-	print(f"A potencia de {a} elevado a {b} é {c}")
+		print(f"A potencia de {a} elevado a {b} é {c}")
 
-	input()
-elif operacao == 6:
-	#RADICIACAO
-	print("\n\n")
-	print("#########################################")
-	print("                 RADICIACAO              ")
-	print("#########################################")
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	elif operacao == 6:
+		#RADICIACAO
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("#########################################")
+		print("                 RADICIACAO              ")
+		print("#########################################")
 
-	a = input("Digite o numero: ")
+		a = input("Digite o numero: ")
 
-	a = int(a)
+		a = int(a)
 
-	b = input("Digite o numero da raiz: ")
+		b = input("Digite o numero da raiz: ")
 
-	b = int(b)
+		b = int(b)
 
-	c = a**(1/b)
+		c = a**(1/b)
 
-	print(f"A raiz {b} de {a} é {c}")
-
-	input()
-else:
-	print("\n")
-	input("Coloca um numero valido parça\nAperta ENTER para fechar")
+		print(f"A raiz {b} de {a} é {c}")
+		
+		print("\n\n")
+		at = input("1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
+	else:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		at = input("Coloca um numero valido parça\n\n1 - Reiniciar\n2 - Fechar\n\nEscolha(numero): ")
+		at = int(at)
+		if at == 1:
+			est = 0
+			os.system('cls' if os.name == 'nt' else 'clear')
+		elif at == 2:
+			est = 1
+			os.system('cls' if os.name == 'nt' else 'clear')
